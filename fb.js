c71f089f1a4c20027c8f6e597c30995b236c3283
Sharb3 <vernard@epftechhubic.org>
2024-04-24 15:45:19 +0200

    Inprogression fb layout

---
+++ fb.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function(){
+    /*$("button").click(function(){
+        $("p:first").hide();
+    })
+ $("button").dblclick(function(){
+    $("p:first").show()
+ })
+ $("p").mouseenter(function(){
+    alert("You entered p1!")
+ })
+
+    $("button").click(function(){
+        $("div").animate({left:"250px"
+    ,opacity: "0.5",
+height:"150px",
+width: "105px"})
+    })
+})*/
